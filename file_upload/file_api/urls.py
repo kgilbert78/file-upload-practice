a2f35e5da6a5a107a4335bec46bd58bc3631dc86
kgilbert78 <kgilbert78@gmail.com>
2024-02-11 17:00:42 -0500

    add serializer, view, urls.py in app and project

---
+++ file_upload/file_api/urls.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import FileViewSet
+
+router = DefaultRouter()
+
+router.register(r"files", FileViewSet, basename="files")
+
+urlpatterns = [
+    path('api/', include(router.urls)),
+]
--- file_upload/file_api/views.py
+++ file_upload/file_api/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import File
+from .serializer import FileSerializer
 
 # Create your views here.
+class FileViewSet(viewsets.ModelViewSet):
+    queryset = File.objects.all()
+    serializer_class = FileSerializer
+    
