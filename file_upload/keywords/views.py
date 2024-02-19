2933e3b3968cacff3f21e403dece0ab5c804b255
kgilbert78 <kgilbert78@gmail.com>
2024-02-18 20:10:20 -0500

    solved keyword problem within multipart formdata requests

--- file_upload/keywords/views.py
+++ file_upload/keywords/views.py
@@ -12,9 +12,14 @@ class FileWithKeywordsViewSet(viewsets.ModelViewSet):
         return files
 
     def add_keywords(self, keyword_data):
+        print(keyword_data)
+        # handling postman input formatted as ["keyword1", "keyword2", "keyword3"]
+        keyword_list = keyword_data.strip('"]["').split('", "')
+        print(keyword_list, type(keyword_list))
         # add keywords in request that aren't already in the db keywords
-        for each_keyword in keyword_data:
+        for each_keyword in keyword_list:
             inDB = Keyword.objects.filter(associated_keyword=each_keyword).exists()
+            print(each_keyword, inDB)
             if inDB == False:
                 new_keyword = Keyword.objects.create(associated_keyword=each_keyword)
                 new_keyword.save()
@@ -32,21 +37,15 @@ class FileWithKeywordsViewSet(viewsets.ModelViewSet):
 
 
         self.add_keywords(data["keyword"])
-
+        
+        keyword_list = data["keyword"].strip('"]["').split('", "')
         # associate the keywords to the new file by keyword text
-        for each_keyword in data["keyword"]:
+        for each_keyword in keyword_list:
             keyword_obj = Keyword.objects.get(associated_keyword=each_keyword)
             # if duplicate keywords are in the db, this line will error:
             # file_api.models.Keyword.MultipleObjectsReturned: get() returned more than one Keyword -- it returned 4!
 
             new_file.keyword.add(keyword_obj)
-
-            # for POST request data format like this:
-            # {
-            #     "name": "filename",
-            #     ...etc...
-            #     "keyword": ["harvesting", "survey"]
-            # }
 
         serializer = FileWithKeywordsSerializer(new_file)
         return Response(serializer.data)
